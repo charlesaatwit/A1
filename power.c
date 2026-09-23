f6bbfeefd25b82e44f390e46206ae50dea5ee593
arleene <charlesa1@wit.edu>
2026-09-23 00:38:26 -0400

    completed power function

--- power.c
+++ power.c
@@ -1,7 +1,17 @@
-
 /**
- * TODO: Complete and document
+ * Calculates x raised to the power of y.
  */
 int power(int x, int y) {
-    return 1;
+
+    if (x < 0 || y < 0) {
+        return 0;
+    }
+
+    int result = 1;
+
+    for (int i = 0; i < y; i++) {
+        result = result * x;
+    }
+
+    return result;
 }
