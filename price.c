ab3ccbb6c2fbabd42976759b3c9b0c3dda78c862
arleene <charlesa1@wit.edu>
2026-09-23 00:34:06 -0400

    Completed price function

--- price.c
+++ price.c
@@ -3,5 +3,18 @@
  * TODO: Complete and document
  */
 int price(int peritem, int count, int bulk_limit) {
-    return 0;
+   
+    if (peritem < 0 || count < 0 || bulk_limit < 0) {
+        return 0;
+    }
+
+    if (count <= bulk_limit) {
+        return peritem * count;
+    }
+
+    int regular_price = peritem * bulk_limit;
+    int extra_items = count - bulk_limit;
+    int discount_price = extra_items * (peritem / 2);
+
+    return regular_price + discount_price;
 }
