3c8e91b3b1569f30c35729c23b2a1937665e86b6
arleene <charlesa1@wit.edu>
2026-09-23 00:50:08 -0400

    Completed max diff function

--- max_diff.c
+++ max_diff.c
@@ -1,8 +1,27 @@
 #include <stdlib.h> // For NULL
 
 /**
- * TODO: Complete and document
+ * Finds the difference between the largest and smallest values in an array.
  */
 int max_diff(int* array, int alen) {
-    return 1;
+
+    if (array == NULL || alen <= 1) {
+        return 0;
+    }
+
+    int min = array[0];
+    int max = array[0];
+
+    for (int i = 1; i < alen; i++) {
+
+        if (array[i] < min) {
+            min = array[i];
+        }
+
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+
+    return max - min;
 }
